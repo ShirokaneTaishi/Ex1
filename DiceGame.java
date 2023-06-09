3bce8d0dcfbf7e962e30d88a18bbf69ab41b132f
ShirokaneTaishi <109419157+ShirokaneTaishi@users.noreply.github.com>
2023-06-09 11:32:33 +0900

    Dice game: initial revision

---
+++ DiceGame.java
@@ -0,0 +1,15 @@
+class DiceGame{
+	
+	public static void main(String[] args) {
+		int i=0,total=0;
+		
+		System.out.println("Rolling dice...");
+		for(i=0;i<2;i++) {
+			int j=(int)Math.ceil(Math.random()*6);
+			total+=j;
+			System.out.println("Dice "+i+": "+j);
+		}
+		
+		System.out.println("Total value: "+total);
+	}
+}
